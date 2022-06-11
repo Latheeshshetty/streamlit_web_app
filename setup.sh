4a2fbbea8184e8352bae1dbb2fec7672221bdf4c
Latheeshshetty <74148127+Latheeshshetty@users.noreply.github.com>
2022-06-11 13:16:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
+mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"latheeshsshetty12.subs@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
